77677a0171e7be2c01675ed5e5a256a626100a02
alkis axos <alkisaxos@gmail.com>
2023-04-06 18:04:37 +0300

    Calculations done front

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Home from "./screens/Home";
 import Calc from "./screens/Calculations";
 import { Routes, Route } from "react-router-dom";
 
