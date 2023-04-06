e2313b3c9993d7224115a15c359d33b38c4e9837
alkis axos <alkisaxos@gmail.com>
2023-04-06 15:10:45 +0300

    first steps

---
+++ src/screens/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="column container">
+      <h1>Welcome !</h1>
+      <button onClick={NextPage}>Next</button>
+    </div>
+  );
+}
+export default Home;
