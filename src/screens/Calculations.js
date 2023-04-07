9ed29b74c7b40acb531b54df988084556c12f726
alkis axos <alkisaxos@gmail.com>
2023-04-07 13:15:21 +0300

    game inserted

--- src/screens/Calculations.js
+++ src/screens/Calculations.js
@@ -4,42 +4,42 @@ import "./Calculations.css";
 function Calculations() {
   //Result for breaker
   return (
-    <div className="box">
-      <div>
-        <a href="/">
-          <button className="secondButton h3">HOME</button>
-        </a>
-        <a href="/game">
-          <button className="secondButton h3">GAME</button>
-        </a>
+    <div>
+      <a href="/">
+        <button className="secondButton h3">HOME</button>
+      </a>
+      <a href="/game">
+        <button className="secondButton h3">GAME</button>
+      </a>
+      <div className="box">
+        <h1 className="h1">CALCULATE</h1>
+        <form className="column">
+          <input
+            className="labels input h2"
+            type="text"
+            id="Ρεύμα"
+            placeholder="Ρεύμα"
+            required
+          />
+          <input
+            className="labels input h2"
+            type="Text"
+            id="Τάση"
+            placeholder="Τάση"
+            required
+          />
+          <input
+            className="labels input h2"
+            type=""
+            id="Ισχύς"
+            placeholder="Ισχύς"
+            required
+          />
+          <button className="mainButton h3" onClick={""}>
+            Υπολογισμός
+          </button>
+        </form>
       </div>
-      <h1 className="h1">CALCULATE</h1>
-      <form className="column">
-        <input
-          className="labels input h2"
-          type="text"
-          id="Ρεύμα"
-          placeholder="Ρεύμα"
-          required
-        />
-        <input
-          className="labels input h2"
-          type="Text"
-          id="Τάση"
-          placeholder="Τάση"
-          required
-        />
-        <input
-          className="labels input h2"
-          type=""
-          id="Ισχύς"
-          placeholder="Ισχύς"
-          required
-        />
-        <button className="mainButton h3" onClick={""}>
-          Υπολογισμός
-        </button>
-      </form>
     </div>
   );
 }
