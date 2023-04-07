b3b60fe8234c79864c5bfbe0ce7226f19e7e0a41
alkis axos <alkisaxos@gmail.com>
2023-04-07 13:51:12 +0300

    home updated

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,10 +3,16 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div className="columncontainer">
+    <div className="column">
       <h1 className="h1">A Job Presentation</h1>
       <a href="/calculation">
         <button className="secondButton h3">Calculator</button>
+      </a>
+      <a href="/game">
+        <button className="secondButton h3">GAME</button>
+      </a>
+      <a href="/mgame">
+        <button className="secondButton h3">BUZZER</button>
       </a>
     </div>
   );
