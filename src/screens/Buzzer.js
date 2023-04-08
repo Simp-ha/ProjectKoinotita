7863db433ea7ec9c08369a9a1bfa7861af516a16
alkis axos <alkisaxos@gmail.com>
2023-04-08 13:39:13 +0300

    buzzer indicator done

--- src/screens/Buzzer.js
+++ src/screens/Buzzer.js
@@ -1,13 +1,29 @@
 import "./Buzzer.css";
+import { useState } from "react";
 function Buzzer() {
+  const [winner, setWinner] = useState("");
+  const A = (e) => {
+    setWinner("A");
+    e.preventDefault();
+  };
+  const B = (e) => {
+    if (B) setWinner("B");
+    e.preventDefault();
+  };
+
   return (
-    <div>
+    <div className="column">
       <a href="/">
         <button className="secondButton h3">HOME</button>
       </a>
+      {winner}
       <div className="container">
-        <button className="ButtonA">A</button>
-        <button className="ButtonB">B</button>
+        <button className="ButtonA" onClick={A}>
+          A
+        </button>
+        <button className="ButtonB" onClick={B}>
+          B
+        </button>
       </div>
     </div>
   );
