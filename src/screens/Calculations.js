6c5565c07a0a7751917fcf653b3e2c7b77e88082
alkis axos <alkisaxos@gmail.com>
2023-04-08 10:51:44 +0300

    calculator completed

--- src/screens/Calculations.js
+++ src/screens/Calculations.js
@@ -4,38 +4,54 @@ import "./Calculations.css";
 
 function Calculations() {
   //Result for breaker
-  const [message, setMessage] = useState("");
-  const calculate = (event) => {
+  const [Volts, setMessage] = useState("");
+  const [Watts, setMessage2] = useState("");
+  const [result, setResult] = useState("");
+  const [isShown, setIsShown] = useState(false);
+
+  const handleChange = (event) => {
     setMessage(event.target.value);
     console.log("value is: ", event.target.value);
   };
+  const handleChange2 = (event2) => {
+    setMessage2(event2.target.value);
+    console.log("value2 is: ", event2.target.value);
+  };
+
+  const calculate = (e) => {
+    setResult(Watts / Volts);
+    e.preventDefault();
+    setIsShown(true);
+  };
   return (
     <div className="box">
-      <div>
+      <div className="column">
         <a href="/">
           <button className="secondButton h3">HOME</button>
         </a>
         <h1 className="h1">CALCULATE</h1>
+        {isShown && (
+          <div className="box labels h3">
+            Η επιθυμιτή ασφάλεια αντέχει ρεύμα {result} A
+          </div>
+        )}
         <form className="column">
-          <output
-            className="labels input h2"
-            type="text"
-            id="Ρεύμα"
-            placeholder="Ρεύμα"
-            required
-          />
           <input
             className="labels input h2"
             type="Text"
-            id="Τάση"
-            placeholder="Τάση"
+            id="Τάση "
+            placeholder="Τάση σε Volt"
+            onChange={handleChange}
+            value={Volts}
             required
           />
           <input
             className="labels input h2"
             type="Text"
             id="Ισχύς"
-            placeholder="Ισχύς"
+            placeholder="Ισχύς σε Watt"
+            value={Watts}
+            onChange={handleChange2}
             required
           />
           <button className="mainButton h3" onClick={calculate}>
